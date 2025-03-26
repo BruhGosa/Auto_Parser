96c24c9419f91a0ce41b83c187f5369247d23bb9
BruhGosa0712 <113544284+BruhGosa@users.noreply.github.com>
2025-03-26 11:04:08 +0300

    Delete autospot_scrapy/run.py

--- autospot_scrapy/run.py
+++
@@ -1,8 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from autospot_scrapy.spiders.autospot_spider import AutospotSpider
-
-if __name__ == "__main__":
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(AutospotSpider)
-    process.start()
